b8cf53b51767cfb3a19130d106db191778a89296
Alex Chaplianka <alexettelis@gmail.com>
2018-11-14 09:45:51 +0200

    Stupid board

--- go-ui/src/App.js
+++ go-ui/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Cell from './Board.js'
+import Board from './Board.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Let's Go! </h1>
-        <Cell piece="White"></Cell>
+        <Board></Board>
       </div>
     );
   }
--- go-ui/src/Board.js
+++ go-ui/src/Board.js
@@ -15,4 +15,28 @@ class Cell extends Component {
     }
 }
 
-export default Cell
+class Board extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            size: this.props.size,
+            data: emptySquare(this.props.size)
+        }
+    }
+    render(){
+        return (
+           <ul>
+               <li><Cell piece="White"></Cell></li>
+               <li><Cell piece="Black"></Cell></li>
+               <li><Cell piece="Green"></Cell></li>
+           </ul> 
+        )
+    }
+}
+
+
+function emptySquare(size){
+    return new Array(size).fill(0).map(() => new Array(size).fill(0));
+}
+
+export default Board

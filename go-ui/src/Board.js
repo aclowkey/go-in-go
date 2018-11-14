c4f7279efcdc81e75f0d88c49b83d3276ca4981d
Alex Chaplianka <alexettelis@gmail.com>
2018-11-14 10:28:42 +0200

    Positional click handling

--- go-ui/src/Board.js
+++ go-ui/src/Board.js
@@ -5,12 +5,17 @@ class Cell extends Component {
     constructor(props){
         super(props)
         this.state = {
-            piece: this.props.piece
+            piece: this.props.piece,
+            onClick: this.props.onClick
         }
     }
     render(){
         return (
-            <div class="square">{this.state.piece}</div>
+            <div 
+                class="square"
+                onClick={this.state.onClick}>
+                {this.state.piece}
+            </div>
         )
     }
 }
@@ -25,13 +30,19 @@ class Board extends Component {
         }
     }
 
+    cellClicked(x, y){
+        return () => {
+            alert("Clicked me on "+x +", "+ y+ "!");
+        }
+    }
+
     render(){
         let board=[]
         for(let i = 0; i < this.state.size; i++){
             let row = [] 
             for(let j = 0; j < this.state.size; j++ ){
                 let piece = this.state.data[i][j].toString();
-                row.push(<Cell piece={piece}></Cell>);
+                row.push(<Cell piece={piece} onClick={this.cellClicked(j, i)}></Cell>);
             }
             board.push(
                 <div style={{display: 'inline-block'}}>

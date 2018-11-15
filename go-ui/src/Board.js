4e894930b45d5b819fe132c3dfa789fa11059ee5
Alex Chaplianka <alexettelis@gmail.com>
2018-11-15 08:32:13 +0200

    Simple turn logic

--- go-ui/src/Board.js
+++ go-ui/src/Board.js
@@ -6,13 +6,13 @@ class Cell extends Component {
         super(props)
         this.state = {
             piece: this.props.piece,
-            onClick: this.props.onClick
+            onClick: this.props.onClick,
         }
     }
     render(){
         return (
             <div 
-                class="square"
+                className="square"
                 onClick={this.state.onClick}>
                 {this.state.piece}
             </div>
@@ -26,13 +26,23 @@ class Board extends Component {
         let size = parseInt(this.props.size)
         this.state = {
             size: size,
-            data: emptySquare(size)
+            data: emptySquare(size),
+            whitesTurn: false
         }
     }
 
     cellClicked(x, y){
         return () => {
-            alert("Clicked me on "+x +", "+ y+ "!");
+           let data = this.state.data.slice()
+           if(data[y][x] !== 0){
+                alert("You can't do that!")
+                return;
+           }
+           data[y][x] =  this.state.whitesTurn ? "White" : "Black";
+           this.setState({
+                data: data,
+                whitesTurn: !this.state.whitesTurn
+           })
         }
     }
 
@@ -41,13 +51,14 @@ class Board extends Component {
         for(let i = 0; i < this.state.size; i++){
             let row = [] 
             for(let j = 0; j < this.state.size; j++ ){
-                let piece = this.state.data[i][j].toString();
-                row.push(<Cell piece={piece} onClick={this.cellClicked(j, i)}></Cell>);
+                let piece =this.state.data[i][j];
+                row.push(<Cell key={i+","+j} piece={piece} onClick={this.cellClicked(j, i)}></Cell>);
             }
+
             board.push(
-                <div style={{display: 'inline-block'}}>
+                <div key={i} style={{display: 'inline-block'}}>
                     {row}
-                </div>
+                </div> 
             )
         }
         return (

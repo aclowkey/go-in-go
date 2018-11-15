280dae1fa3f15f9577d7d61c4fc3be949476e09d
Alex Chaplianka <alexettelis@gmail.com>
2018-11-15 09:40:06 +0200

    Board manages that state

--- go-ui/src/Game.js
+++ go-ui/src/Game.js
@@ -7,23 +7,6 @@ class Game extends Component {
     let size = parseInt(this.props.size);
     this.state = {
       size: size,
-      data: emptySquare(size),
-      whitesTurn: false
-    };
-  }
-
-  cellClicked(x, y) {
-    return () => {
-      let data = this.state.data.slice();
-      if (data[y][x] !== "Empty") {
-        alert("You can't do that!");
-        return;
-      }
-      data[y][x] = this.state.whitesTurn ? "white" : "black";
-      this.setState({
-        data: data,
-        whitesTurn: !this.state.whitesTurn
-      });
     };
   }
 
@@ -31,14 +14,10 @@ class Game extends Component {
     return (
       <div>
         <h1>Let's Go!</h1>
-        <Board size={this.state.size} data={this.state.data} />
+        <Board size={this.state.size}  />
       </div>
     );
   }
-}
-
-function emptySquare(size) {
-  return new Array(size).fill("Empty").map(() => new Array(size).fill("Empty"));
 }
 
 export default Game;

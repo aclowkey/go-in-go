fe23a08237cfc958598f6bf93fd4a8e794be31b7
Alex Chaplianka <alexettelis@gmail.com>
2018-11-17 10:46:29 +0200

    Adding game session and some permission checks

--- http_server.go
+++ http_server.go
@@ -8,17 +8,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GameSession has the game, and the players
+type GameSession struct {
+	game      *Game
+	player1id *int
+	player2id *int
+	public    bool
+}
+
+func (session *GameSession) isReady() bool {
+	return session.player2id != nil
+}
+
 // HTTPServer is a Go-in-go server in HTTP
 type HTTPServer struct {
 	port  int
-	games map[int]*Game
+	games map[int]*GameSession
 }
 
 // MakeHTTPServer creates the server
 func MakeHTTPServer(port int) *HTTPServer {
 	return &HTTPServer{
 		port,
-		make(map[int]*Game),
+		make(map[int]*GameSession),
 	}
 }
 
@@ -31,8 +43,15 @@ func (server *HTTPServer) start() {
 	})
 	r.POST("/game", func(c *gin.Context) {
 		gameID := 5
-		server.games[gameID] = CreateGame(9, 4.5)
+		game := CreateGame(9, 4.5)
 		sessionID := rand.Intn(10000)
+		gameSession := &GameSession{
+			game,
+			&sessionID,
+			nil,
+			false,
+		}
+		server.games[gameID] = gameSession
 		c.Header("gameID", strconv.Itoa(gameID))
 		c.Header("sessionID", strconv.Itoa(sessionID))
 		c.JSON(200, gin.H{
@@ -40,6 +59,7 @@ func (server *HTTPServer) start() {
 		})
 	})
 	r.GET("/game/:id", func(c *gin.Context) {
+
 		gameIDParam := c.Param("id")
 		gameID, err := strconv.Atoi(gameIDParam)
 		if err != nil {
@@ -48,16 +68,33 @@ func (server *HTTPServer) start() {
 			})
 			return
 		}
-		game, ok := server.games[gameID]
+		gameSession, ok := server.games[gameID]
 		if !ok {
 			c.JSON(404, gin.H{
 				"message": fmt.Sprintf("Game %d not found", gameID),
 			})
 			return
 		}
+		if !gameSession.public {
+			sessionIDHeader := c.GetHeader("sessionID")
+			sessionID, err := strconv.Atoi(sessionIDHeader)
+			if err != nil {
+				// Either expired, or not authorized, or invalid
+				c.JSON(404, gin.H{
+					"message": "Not allowed to access the game",
+				})
+				return
+			}
+			if !(sessionID == *gameSession.player1id || sessionID == *gameSession.player2id) {
+				c.JSON(404, gin.H{
+					"message": "Not allowed to access the game",
+				})
+				return
+			}
+		}
+
 		c.JSON(200, gin.H{
-			"board": game.board.data,
-			"turn":  game.turn.String(),
+			"turn": gameSession.game.turn,
 		})
 	})
 	r.POST("/game/:id", func(c *gin.Context) {
@@ -69,18 +106,24 @@ func (server *HTTPServer) start() {
 			})
 			return
 		}
-		game, ok := server.games[gameID]
+		gameSession, ok := server.games[gameID]
 		if !ok {
 			c.JSON(404, gin.H{
 				"message": fmt.Sprintf("Game %d not found", gameID),
 			})
 			return
 		}
+		if gameSession.isReady() {
+			c.JSON(400, gin.H{
+				"message": "Game has already started",
+			})
+			return
+		}
 		sessionID := rand.Intn(10000)
+		gameSession.player2id = &sessionID
 		c.Header("sessionID", strconv.Itoa(sessionID))
 		c.JSON(200, gin.H{
-			"board": game.board.data,
-			"turn":  game.turn.String(),
+			"turn": gameSession.game.turn.String(),
 		})
 	})
 	r.Run(fmt.Sprintf(":%d", server.port))

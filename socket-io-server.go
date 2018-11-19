c089ca4141bc69f7406355a347e5da3089d99ab4
Alex Chaplianka <alexettelis@gmail.com>
2018-11-19 10:33:35 +0200

    Better game movement handling

--- socket-io-server.go
+++ socket-io-server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -12,7 +13,50 @@ import (
 )
 
 type SocketIOServer struct {
-	port int
+	port         int
+	gameSessions map[string]*IOGameSession
+}
+
+type Player struct {
+	name   string
+	piece  Piece
+	socket socketio.Socket
+}
+
+type IOGameSession struct {
+	game    *Game
+	player1 *Player
+	player2 *Player
+}
+
+func (gameSession *IOGameSession) join(player *Player) (hasPlace bool) {
+	hasPlace = true
+	if gameSession.player1 == nil {
+		gameSession.player1 = player
+	} else if gameSession.player2 == nil {
+		gameSession.player2 = player
+	} else {
+		hasPlace = false
+	}
+	return hasPlace
+}
+
+func (gameSession *IOGameSession) ready() bool {
+	return gameSession.player1 != nil &&
+		gameSession.player2 != nil
+}
+
+func (gameSession *IOGameSession) boardChanged() {
+	gameSession.player1.socket.Emit("board_changed")
+	gameSession.player2.socket.Emit("board_changed")
+}
+
+func MakeSocketIOServer(port int) *SocketIOServer {
+	gameSessions := make(map[string]*IOGameSession)
+	return &SocketIOServer{
+		port,
+		gameSessions,
+	}
 }
 
 func (server *SocketIOServer) Start() {
@@ -21,35 +65,79 @@ func (server *SocketIOServer) Start() {
 		log.Fatal(err)
 		return
 	}
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
+	log.Level = log.LevelDebug
 
-	gameSessions := make(map[string]bool)
-	socketServer.On("connection", func(so socketio.Socket) {
-		var gameID string
-		playerRole := "white"
-		gameIDparams, ok := so.Request().URL.Query()["gameID"]
-		if !ok && len(gameID) != 1 {
-			gameID = strconv.Itoa(r1.Int())
-			gameSessions[gameID] = true
-			log.Infof("Game %s created \n", gameID)
-		} else {
-			gameID = gameIDparams[0]
-			_, found := gameSessions[gameID]
-			if !found {
-				log.Errorf("Player tried to joined room which doesn't exist!")
-				return
-			}
-			playerRole = "black"
-		}
-		playerID := strconv.Itoa(r1.Int())
-		so.Join(gameID)
-		log.Infof("Player %s (%s) join game (%s)", playerRole, playerID, gameID)
-		// log.Infof("Request: %s\n", )
-		// so.Emit("joined_game", fmt.Sprintf("Joined game %s", gameID))
-	})
-
+	socketServer.On("connection", server.handleConnection)
 	http.Handle("/", socketServer)
 	log.Infof("Listening for socket-io on :%d\n", server.port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", server.port), nil))
+}
+
+func (server *SocketIOServer) handleConnection(so socketio.Socket) {
+	// Handle user connecting to server by either joining or creating a game
+	var gameID string
+	var player *Player
+	gameIDParams, ok := so.Request().URL.Query()["gameID"]
+	if ok && len(gameIDParams) == 1 {
+		// Joining an existing game
+		gameID = gameIDParams[0]
+		gameSession, found := server.gameSessions[gameID]
+		if !found {
+			log.Debugf("User attempted to join an invalid game: %s\n", gameID)
+			so.Emit("error", "Cannot find game")
+			return
+		}
+		player = &Player{"Player 2", Black, so}
+		hasPlace := gameSession.join(player)
+		if !hasPlace {
+			log.Debugf("User attempted to join an already full game: %s\n", gameID)
+			so.Emit("error", "Room already full")
+			return
+		}
+
+		log.Debugf("[%s] Player joined game", gameID)
+	} else {
+		// Creating a new game - TODO limit capacity?
+		r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
+		gameID = strconv.Itoa(r1.Int())
+		server.gameSessions[gameID] = &IOGameSession{
+			CreateGame(9, 4.5), nil, nil,
+		}
+		player = &Player{"Player 1", White, so}
+		server.gameSessions[gameID].join(player)
+		so.Emit("message", fmt.Sprintf("gameid=\"%s\"", gameID))
+	}
+	gameSession := server.gameSessions[gameID]
+
+	// Waiting for second player to join so we'll be ready
+	// Maybe better approach is the server will notify itself on game_started?
+	for !gameSession.ready() {
+		// TODO timeout
+	}
+
+	// Game is ready, handle movement logic
+	so.Emit("game_started", gameSession.game.board.Pieces())
+	so.On("move", server.handleMove(gameID, gameSession, player))
+}
+
+func (server *SocketIOServer) handleMove(gameID string, gameSession *IOGameSession, player *Player) interface{} {
+	game := gameSession.game
+	return func(data string) {
+		var position Position
+		err := json.Unmarshal([]byte(data), &position)
+		if err != nil {
+			player.socket.Emit("error", "Invalid syntax!")
+			return
+		}
+
+		result, err := game.Move(&Move{position.X, position.Y, player.piece})
+		if result == Ok {
+			gameSession.boardChanged()
+			player.socket.Emit("message", fmt.Sprintf("Thank you mr, %s", player.name))
+			log.Debugf(game.board.String(false))
+			log.Debugf("[%s] Player %s moved %s\n", gameID, player.piece)
+		} else {
+			player.socket.Emit("error", err.Error())
+		}
+	}
 }
